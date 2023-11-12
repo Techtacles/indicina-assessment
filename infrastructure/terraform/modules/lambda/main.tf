44b6ed5168da86d79f3bed5fa0d0ba5c8f5141c3
Techtacles <emmanuel.offisong@data2bots.com>
2023-11-12 08:09:58 +0100

    Modifying the infra to remove handler in lambda

--- infrastructure/terraform/modules/lambda/main.tf
+++ infrastructure/terraform/modules/lambda/main.tf
@@ -3,14 +3,14 @@ resource "aws_lambda_function" "lambda_fn" {
   # path.module in the filename.
   function_name = var.lambda_fn_name
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = var.lambda_handler
+  #handler       = var.lambda_handler
   image_uri     = "${var.ecr_image_uri}@${var.ecr_sha}"
   package_type  = "Image"
   timeout       = 600
 
   #source_code_hash = data.archive_file.lambda.output_base64sha256
 
-  runtime = "python3.9"
+  #runtime = "python3.9"
   environment {
     variables = {
       S3_BUCKET_NAME    = var.s3_bucket
